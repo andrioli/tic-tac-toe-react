ad641cca20acf59e473d7e6f2823c28ff752ded0
Roberto Araujo <roberto.andrioli@gmail.com>
2017-08-20 16:12:36 -0300

    Pull the status label up from Board to Game

--- src/index.js
+++ src/index.js
@@ -21,17 +21,8 @@ class Board extends React.Component {
   }
 
   render() {
-    const winner = calculateWinner(this.props.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.props.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -75,13 +66,21 @@ class Game extends React.Component {
   }
 
   render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares={this.state.squares} xIsNext={this.state.xIsNext} onClick={this.handleClick}/>
+          <Board squares={this.state.squares} onClick={this.handleClick}/>
         </div>
         <div className="game-info">
-          <div>{/* status */}</div>
+          <div>{status}</div>
           <ol>{/* TODO */}</ol>
         </div>
       </div>
